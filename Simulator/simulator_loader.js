cd8fe270f536ef88d4ea69065a661b3caeaeac7c
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-03-01 06:57:06 -0300

    Using actual changelog link on simulator page.

--- Simulator/simulator_loader.js
+++ Simulator/simulator_loader.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 });
 
 function loadChangelog() {
-    var changelog_url = "https://raw.githubusercontent.com/unball/simulator_unity/master/README.md";
+    var changelog_url = "https://raw.githubusercontent.com/gabrielnaves/TG/master/shp_sim/changelog.md";
     $("#changelog").load(changelog_url, function() {
         var converter = new showdown.Converter();
         var text = $("#changelog").html();
