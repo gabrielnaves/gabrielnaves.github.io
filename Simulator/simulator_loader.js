3bc410b3ee568f14166d0199b6ee8daf3fe29497
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-03-01 05:10:07 -0300

    Implementing the changelog on simulator page. Using dummy changelog for now.

--- Simulator/simulator_loader.js
+++ Simulator/simulator_loader.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
+    loadChangelog();
 });
+
+function loadChangelog() {
+    var changelog_url = "https://raw.githubusercontent.com/unball/simulator_unity/master/README.md";
+    $("#changelog").load(changelog_url, function() {
+        var converter = new showdown.Converter();
+        var text = $("#changelog").html();
+        $("#changelog").html(converter.makeHtml(text));
+    });
+}
 
 function loadSimulator() {
     var src_url = "https://itch.io/embed-upload/784947?color=333333";
